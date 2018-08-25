6a582b86beb65d25aca791a9ff1229a0dbaef9a5
Hungdt-ibl <hungdt@blockchainlab.asia>
2018-08-25 23:13:02 +0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,42 +1,26 @@
 importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js");
-let config = {
-   messagingSenderId: "447867740573"
-};
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(payload => {
-   const title = payload.notification.title;
-   console.log('payload', payload.notification.icon);
-   const options = {
-      body: payload.notification.body,
-      icon: payload.notification.icon
-   }
-   return self.registration.showNotification(title, options);
-})
 
-self.addEventListener("notificationclick", function(event) {
-  const clickedNotification = event.notification;
-  clickedNotification.close();
-  const promiseChain = clients
-      .matchAll({
-          type: "window",
-          includeUncontrolled: true
-       })
-      .then(windowClients => {
-          let matchingClient = null;
-          for (let i = 0; i < windowClients.length; i++) {
-              const windowClient = windowClients[i];
-              if (windowClient.url === feClickAction) {
-                  matchingClient = windowClient;
-                  break;
-              }
-          }
-          if (matchingClient) {
-              return matchingClient.focus();
-          } else {
-              return clients.openWindow(feClickAction);
-          }
-      });
-      event.waitUntil(promiseChain);
+/* Now initialize firebase app in the servie worker.
+ *
+ */
+firebase.initializeApp({
+    apiKey: 'AIzaSyAS_NLgtV381v_hu43FgSlguEGAwki4sDI',
+    authDomain: 'zerg-chatme-d8bba.firebaseapp.com',
+    databaseURL: 'https://zerg-chatme-d8bba.firebaseio.com',
+    projectId: 'zerg-chatme-d8bba',
+    storageBucket: 'zerg-chatme-d8bba.appspot.com',
+    messagingSenderId: '447867740573'
+}); //These are example configuration value
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  const notificationTitle = ﻿payload.data.title;
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: payload.data.icon
+  };
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
