fcc14c10796abef56152a6185928246c4930ad65
Hungdt-ibl <hungdt@blockchainlab.asia>
2018-08-27 00:06:44 +0700

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/4.2.0/firebase-messaging.js");
 
 /* Now initialize firebase app in the servie worker.
  *
